8b1f692e84a76620e5a26f43f07694e1d704e7bf
ray35941 <ray35941@gmail.com>
2025-12-11 18:59:59 -0600

    Update Utils.java
    remove code smell (useless method)

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -11,8 +11,5 @@ public class Utils {
         if (age < 0) return false;
         else return true;  // Bug: Allows age > 120
     }
-    // Unused method (code smell)
-    public static void printMessage(String msg) {
-        System.out.println(msg);
-    }
+    
 }
